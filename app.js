d1894ef2384b853058bfc3aba3941d7d6913c613
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-05-10 23:44:43 -0700

    ok

---
+++ app.js
@@ -0,0 +1,664 @@
+
+//var num1 = 5 //6  7  8  9
+//var num2 = 4 //5   6
+//var userName = prompt('what is your good name')
+
+//         5       5        4       5       6        6         8      8       8
+//var ans = num1 + num1++ + num2 + ++num2 + num1++ + ++num2 + ++num1 + num1 + num1++
+
+// alert(num1)
+
+
+// var user = prompt("enter your good name")
+// alert('thanks for comming'+' '+ user)
+
+// alert(total)
+//document.getElementById('demo').innerHTML = userName
+
+
+
+// if (2 > 3) {
+//     alert('bhai sahb garmi he bht')
+// }
+// else {
+//     alert('bhai garmi nh he')
+
+// }
+
+// var num1 = 4
+// var num2 = 5
+// var num3 = 6
+// var num4 = num1 < num2
+// var num5 = num3 <= num1
+// var num6 = (num4 && num5)
+// alert(num6)
+// if (num6 = false) {
+//     prompt('write your name')
+// } else{
+//     alert('wrong answer')
+// }
+
+// var firstName = prompt('enter your first name')
+// var lastName = prompt('enter your last name')
+// var fullName = firstName + ' ' + lastName
+
+// var email = prompt('enter your email')
+// var password = prompt('enter your password')
+// if (email == "hasnainkhatri45@gmail.com" && password == "03143363710") {
+//     alert('log in sucsessfully')
+// } else if (email == 'hasnainkhatri45@gmail.com') {
+//     console.log('your password is incorrect');
+
+// } else if (password == '03143363710') {
+//     console.log('your email is incorrect ');
+// }
+// else {
+//     alert('log in feaild')
+//     console.log(email);
+//     console.log(password);
+
+// }
+
+// var email = prompt('enter your email')
+// var password = prompt('enter your password')
+// if (email == "hasnainkhatri45@gmail.com" && password == "03143363710") {
+//     alert('log in sucsessfully')
+//     console.log('you are log in now');
+// } else if (email == 'hasnainkhatri45@gmail.com') {
+//     console.log('your password is incorrect');
+
+// } else if (password == '03143363710') {
+//     console.log('your email is incorrect ');
+// }
+// else {
+//     alert('log in feaild')
+//     console.log(email);
+//     console.log(password);
+
+// }
+// var a = 15
+// var b = 5
+// b += a
+// alert(b)
+
+// var c = prompt('write to check even or odd number')
+// if (c < 25) {
+//     if (c % 2 == 0) {
+//         alert('this is even number')
+//     } else (
+//         alert('this is odd number')
+
+//     )
+
+// }else{
+//     alert(c+' '+'is greater than 25')
+// }
+
+
+// var num1 = prompt('Entera number jiska apko table chahiye')
+// var t1 = num1 * 1 ;
+// var t2 = num1 * 2 ;
+// var t3 = num1 * 3 ;
+// var t4 = num1 * 4 ;
+// var t5 = num1 * 5 ;
+// var t6 = num1 * 6 ;
+// var t7 = num1 * 7 ;
+// var t8 = num1 * 8 ;
+// var t9 = num1 * 9 ;
+// var t10 = num1 * 10 ;
+
+
+// document.write('6767676776')
+// document.write(t1)
+// document.write(t2)
+// document.write(t3)
+// document.write(t4)
+// document.write(t5)
+// document.write(t6)
+// document.write(t7)
+// document.write(t8)
+// document.write(t9)
+// document.write(t10)
+
+
+// console.log(t1)
+// console.log(t2)
+// console.log(t3)
+// console.log(t4)
+// console.log(t5)
+// console.log(t6)
+// console.log(t7)
+// console.log(t8)
+// console.log(t9)
+// console.log(t10)
+
+
+
+
+// var num = [2, 20, 50, 10, 35, 15]
+// var numChek = prompt('Enter Your Roll Number')
+// var done = false
+// for (var i = 0; i < num.length; i++) {
+//     if (num[i]==numChek) {
+//         done = true
+//     break
+//     }
+
+// }
+// if (done == true) {
+//     alert('you are found')
+// } else {
+//     alert('you are not found')
+// }
+
+
+
+// var num = ['rashid', 'hanif', 'hasnain' , 'haseeb']
+// var numChek = prompt('check karo')
+// var done = false
+
+// for (var i = 0; i < num.length; i++) {
+//     if (num[i] == numChek) {
+//         done = true
+//         break
+//     }
+// }
+// if (done == true) {
+//     alert(numChek + ' ' + 'Is Avalible')
+// } else {
+//     alert(numChek + ' ' + 'Not Avalible')
+// }
+// var names = ['hasnain', 'haseeb', 'hanif', 'rashid']
+// var checker = prompt('Enter Name Here')
+// var approve = false
+
+// for (var fig = 0; fig < names.length; fig++) {
+//     if (names[fig] == checker) {
+//         approve = true
+//         break
+//     }
+// }
+// if (approve == true) {
+//     alert(checker + ' ' + 'Is Avalible Here')
+// } else {
+//     alert(checker + ' ' + 'Is Not Avalible Here')
+// }
+// var names = ['hanif', 'rashid', 'hasnain', 'haseeb']
+// var checker = prompt('enter name here')
+// var approval = false
+// for (let loop = 0; loop < names.length; loop++) {
+//     if (names[loop] == checker) {
+//         approval = true
+//         break
+//     }
+// }
+// if (approval == true) {
+//     alert(checker + ' ' + 'Is Avalibe Here')
+// } else {
+//     alert(checker + ' ' + 'Is Not Avalibe Here')
+// }
+
+//     var party_Names = ['shoky garments' , 'hello boys garments' , 'mm garments' , 'treat garments']
+//     var checker = prompt('Enter Party Name Here')
+//     var founder = false
+//     for (let index = 0; index < party_Names.length; index++) {
+//         if(party_Names[index] == checker)
+//         founder = true
+//     }
+// if(founder == true){
+//     alert('ok ki report he')
+// }else{
+//     alert('noo ki report he')
+
+// }
+
+
+
+
+// for(var i = 100 ; i >= 1 ; i--){
+//     document.write(i , '<br>')
+// }
+
+
+// for (i = 10; i <= 100; i += 10) {
+//     document.write(i , '<br>')
+//     // break
+// }
+
+// var count = [12, 20, 30, 40, 50, 60, 77, 80, 95, 101]
+// for (var i = 0; i < count.length; i++) {
+
+// }
+
+
+
+// var sum = 0
+// for (var index = 0; index < 10; index++) {
+
+//     sum = 5
+// }
+
+// console.log(sum)
+
+///////////////////////////   IMPORTANT    Name Alag karne
+
+
+// var names1 = ['hasnain', 'abdul Hai', 'ahmed Ali', 'kashif', 'ashfaq'];
+
+// for (var i = 0; i < names1.length; i++) {
+//     if (names1[i][0] == 'a') {
+//         document.write('ok <br>')
+//     }else{
+//         document.write('not ok <br>')
+
+//     }
+// }
+
+
+
+//////////   Extra Assigment In ARRAY   \\\\\\\\
+
+
+
+//////////        1st Assigment         \\\\\\\\
+
+
+
+// var arr = ['hasnain', 'Haseeb', 'Rehman', 12, true, 'ammar', 'AbdulHai']
+// document.write('<h2>Array Before (Slice)</h2> <br>')
+// document.write(arr, '<br>')
+
+// var newArr = arr.slice(2, 6)
+// document.write('<h2>Array After (Slice)</h2> <br>')
+// document.write(newArr)
+
+
+
+//////////        2nd Assigment         \\\\\\\\
+
+
+
+// var arr = ['hasnain', 'Haseeb', 'Hamza', 'Daniyal', 'Rizwan', 'Kashif', 'Ahmed']
+// document.write('<h2>Array Before (Splice)</h2> <br>')
+// document.write(arr, '<br>')
+
+// document.write('<h2>Array After(Splice)</h2> <br>')
+// var newArr = arr.splice(2, 6)
+// document.write(newArr)
+
+
+
+//////////        3rd Assigment         \\\\\\\\
+
+
+
+// var arr = ['hasnain', 'Haseeb', 'Hamza', 'Daniyal', 'Rizwan', 'Kashif', 'Ahmed']
+// document.write('<h2>Array Before (Shift)</h2> <br>')
+// document.write(arr, '<br>pro')
+
+// document.write('<h2>Array After (Shift)</h2> <br>')
+// var hasnain = arr.shift()
+
+
+
+//////////        4th Assigment         \\\\\\\\
+
+
+// var arr = ['hasnain', 'Haseeb', 'Hamza', 'Daniyal', 'Rizwan', 'Kashif', 'Ahmed']
+// var user = prompt('enetr namber')
+// alert(arr[user - 1])
+
+
+
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j] + "<br>");
+//         document.write(fullNames, '<br>')
+
+//     }
+// }
+
+// var name = prompt('name My')
+// name = name.toUpperCase()
+
+// document.write(name)
+
+
+
+// document.write('<center>')
+//     for (let a = 1; a <= 9; a++) {
+//         for (let b = 1; b <= a; b++) {
+//             document.write(a + " ")
+//         }
+//         document.write('<br>')
+//     }
+//     for (let a = 9; a >= 1; a--) {
+//         for (let b = 1; b <= a; b++) {
+//             document.write(a + " ")
+//         }
+//         document.write('<br>')
+//     }
+//     document.write('<br>')
+//     document.write('<br>')
+//     document.write('<br>')
+//     document.write('<br>')
+
+// document.write('<center>')
+
+// let c = ['A', "B", "C", 'D', 'E', 'F', 'G','H','I','J','K']
+// for (let a = 0; a <= 9; a++) {
+//     for (let b = 0; b <= a; b++) {
+//         document.write(c[b] + " ")
+//     }
+//     document.write('<br>')
+// }
+// for (let a = 1; a <= 9; a++) {
+//     for (let b = 1; b <= a; b++) {
+//         document.write(a + " ")
+//     }
+//     document.write('<br>')
+// }
+
+// document.write('</center>')
+
+
+
+
+
+
+// for (i = 0; i < 10; i++) {
+//     for (j = 1; j <= i; j++)
+//         document.write(j, '<br>')
+// }
+
+
+
+
+
+///////////////// first latter name chakker and combind \\\\\\\\\\\\\\\
+
+
+
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+
+
+// for (var i = 0; i < firstNames.length; i++) {
+//     for (var j = 0; j < lastNames.length; j++) {
+//         if (firstNames[i][0] == lastNames[j][0]) {
+//             fullNames.push(firstNames[i] + lastNames[j] + "<br>")
+
+//         }
+
+//     }
+// }
+
+// document.write(fullNames)
+
+
+// let friuts = ['Apple', 'Banana', 'Orange', 'Mango', 'graps']
+// var user_Deside_Elements = prompt('How Many Friuts You Want')
+// friuts = friuts.slice(0, user_Deside_Elements)
+// document.write(friuts, '<br>')
+
+
+// var i = 1;
+// do {
+//     document.write(i + ') Hasnain <br>')
+//     i++
+// } while (i <= 15)
+
+// var name = 'rizwan'
+// var num = '987654'
+
+// for (i = 5; i >= 0; i--) {
+//     document.write(name[i])
+// }
+
+// document.write('<br>')
+// for (i = 5; i >= 0; i--) {
+//    document.write(num[i])
+//      }
+
+//
+
+// var para = 'my name is hasnain khatri and i am from karachi pakistan and a bussniseman'
+// var find = prompt('search here')
+
+// var flag = false
+// for (var i = 0; i < para.length; i++) {x
+
+//     if (para[i] == find) {
+//         document.write(find + ' is found on index ' + i)
+//         // flag = true
+//         break
+//     }
+
+// }
+
+
+// var para = 'my name is hasnain khatri and i am from karachi pakistan and a bussniseman'
+// var find = prompt('search here')
+// var news = para.indexOf(find)
+// if (news == -1) {
+//     document.write('<h1>' + 'not found')
+//     }
+//     else {
+//         document.write('<h1>' + 'found on index ' + news)
+
+//         }
+
+// <!-- var para = 'my name is hasseb khatri and i am from karachi pakistan and a a bussniseman'
+// var count = 0
+// for(var i = 0 ; i < para.length ; i++){
+//     if(para[i] == "a"){
+//         ++count
+
+
+
+////////////////////   RGB Colour Genrater   \\\\\\\\\\\\\\\\\\\\
+
+
+
+//     }
+// }
+// document.write(count) -->
+
+// var red = Math.round(Math.random() * 255)
+// var green = Math.round(Math.random() * 255)
+// var blue = Math.round(Math.random() * 255)
+// document.write(`rgb (${red}, ${green}, ${blue})`)
+
+
+
+// var num = Math.random() * 999;
+// if(num >= 100){
+
+//     document.write(Math.round(num))
+// }
+
+
+////////////////////   AGE CALCULATOR   \\\\\\\\\\\\\\\\\\\\
+
+
+
+
+// var orignalString = new Date() + ' ';
+// var orignal = new Date();
+// var myBirthDayString = new Date("Sep 30,2004 05:14:13") + ' ';
+// var myBirthDay = new Date("Sep 30,2004 05:14:13");
+// var totalMilliSecond = orignal - myBirthDay;
+// var totalDays = totalMilliSecond / (1000 * 60 * 60 * 24);
+
+
+// document.write('<h1>ToDay Time And Date :-  ' + orignalString.slice(0, 24))
+// document.write('<br>')
+// document.write('My BirthDay Date :- ' + myBirthDayString.slice(0, 24))
+// document.write('<br>')
+// document.write('Total MilliSeconds From My Birth To Now :-  ' + totalMilliSecond)
+// document.write('<br>')
+// document.write('Total Seconds From My Birth To Now :-  ' + totalMilliSecond / 1000)
+// document.write('<br>')
+// document.write('Total Minutes From My Birth To Now :-  ' + totalMilliSecond / (1000 * 60))
+// document.write('<br>')
+// document.write('Total Hours From My Birth To Now :-  ' + totalMilliSecond / (1000 * 60 * 60))
+// document.write('<br>')
+// document.write('Total Days From My Birth To Now :-  ' + totalDays)
+// document.write('<br>')
+// document.write('Total Days From My Birth To Now :-  ' + totalDays/30)
+
+
+
+
+////////////////////   AM PM Checker   \\\\\\\\\\\\\\\\\\\\
+
+
+
+// var now = new Date();
+// var timeOnly = now.getHours();
+// // var timeOnly = 11;
+// if (timeOnly >= 12) {
+//     document.write(timeOnly, ' PM')
+// } else{
+
+//     document.write(timeOnly, ' AM')
+// }
+
+////////////////////   Days Name checker   \\\\\\\\\\\\\\\\\\\\
+
+// var now = new Date()
+// var day = now.getDay()
+// var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// day = days[day]
+// document.write(day)
+
+// function gusse() {
+//     var random = Math.round(Math.random() * 11)
+//     console.log(random)
+// }
+
+
+
+// function game(kashif,ahmed) {
+//     var user = prompt('Enter a Number')
+//     var random = Math.round(Math.random() * 11)
+//     if (user == 6){
+//         alert('Congrateculation ! You WON The Game')
+//     }else{
+//         alert('Try Again Later')
+
+//     }
+// }
+
+
+/////////////   RANDOM PASSWORD GENRATOR \\\\\\\\\\\\\
+
+
+// var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '1', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// var ABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+// var symble = ['@', '#', '$', '%', '&', '*',]
+// var abcNumber = Math.round(Math.random() * 24)
+// var ABCNumber = Math.round(Math.random() * 24)
+// var number = Math.round(Math.random() * 9)
+// var symbleNumber = Math.round(Math.random() * 5)
+// function passwordGenrator() {
+//     var password = [abc[abcNumber], number, ABC[ABCNumber], abc[abcNumber], symble[symbleNumber], number, ABC[ABCNumber]]
+//     password.join(' ')
+//     document.write(password)
+//     // document.write('<br>',abc[abcNumber], number, ABC[ABCNumber], abc[abcNumber], symble[symbleNumber], number, ABC[ABCNumber])
+
+// }
+
+
+
+///////////////////////////       bara name gusseeee
+
+
+
+// var sentence = 'my name is hasnain khatri'
+// var array = sentence.split(' ')
+// var num = 0
+// var big;
+// for (var i = 0; i < array.length; i++) {
+//     if (array[i].length > num) {
+//         num = array[i].length
+//         big = array[i]
+//     }
+
+
+// }
+// document.write(big)
+
+
+
+
+///////////////////////////       aulta karna word
+
+
+// function pword() {
+//     var userWord = prompt('Enter Palindrome Word')
+//     var reverseWord = ''
+//     for (let i = userWord.length - 1; i >= 0; i--) {
+//         reverseWord += userWord[i]
+//     }
+//     if (reverseWord == userWord) {
+//         document.write(`This Is Palindrome Word: ${userWord}`)
+//     } else {
+//         document.write(`Not a Palindrome Word: ${userWord}`)
+//     }
+// }
+// document.write(pword())
+
+
+///////    14 html
+
+
+// function calculateAge() {
+//     var birthYearInput = document.getElementById("birthYear").value;
+//     var birthMonthInput = document.getElementById("birthMonth").value;
+//     var currentYear = new Date().getFullYear();
+//     var currentMonth = new Date().getMonth() + 1;
+//     var ageYear = currentYear - birthYearInput - 1;
+//     var ageMonth = birthMonthInput - currentMonth;
+
+
+//     if (ageYear > 0) {
+//         document.getElementById("result").innerHTML = "Your age is: " + ageYear + " years and " + ageMonth + " Months old.";
+//     } else {
+//         document.getElementById("result").innerHTML = "Please enter a valid year.";
+//     }
+
+// }
+
+
+
+
+
+///////    24 html
+
+// var data = 0
+// function Increment() {
+//     data++
+//     document.getElementById('counter').innerText = data
+
+// }
+
+// function Decrement() {
+//     data--
+//     if(data < 0){
+// data = 0
+//     }
+//     document.getElementById('counter').innerText = data
+// }
+
+// function Reset() {
+//     data = 0
+//     document.getElementById('counter').innerText = data
+
+// }
