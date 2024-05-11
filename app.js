8ff647ac1b402cce05e879c7235979b8e70f6eae
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-05-11 04:10:35 -0700

    ok

--- app.js
+++ app.js
@@ -640,7 +640,7 @@
 
 
 
-///////    24 html
+///////    26 html
 
 // var data = 0
 // function Increment() {
@@ -662,3 +662,90 @@
 //     document.getElementById('counter').innerText = data
 
 // }
+
+
+
+/////////////// IMG Changer      html   37
+
+// var img = document.getElementById('image');
+// function reset() {
+//     img.src = 'https://i1.sndcdn.com/artworks-000202625769-afashx-t500x500.jpg'
+
+// }
+// function car() {
+//     img.src = 'car.jpeg'
+
+// }
+// function bike() {
+//     img.src = 'bike.cms'
+
+// }
+// function airplane() {
+//     img.src = 'plane.jpeg'
+
+// }
+
+
+//////////// A B C D     html 47
+
+
+
+
+
+// var input = document.getElementsByClassName('button')
+// console.log(input);
+
+
+// function abc(letters) {
+//     var number = Math.round(Math.random() * 2)
+//     var output = document.getElementById('main')
+//     var arrA = [['Apple', 'Airplane', 'Ant'], ['Ball', 'Balti', 'Balloon'], ['Can', 'Cash', 'Cat']]
+//     for (var i = 0; i < arrA.length; i++) {
+//         if (arrA[i][0][0] === letters) {
+//             output.innerText = arrA[i][number]
+
+//         } else {
+//             output.innerText = ('ERRor')
+
+//         }
+//     }
+// }
+
+////////////////////////// error
+
+
+function abc(letters) {
+
+    var number = Math.round(Math.random() * 2)
+    var output = document.getElementById('main')
+    var arrA = [['Apple', 'Airplane', 'Ant'], ['Ball', 'Balti', 'Balloon'], ['Can', 'Cash', 'Cat'], ['Dice', 'Donkey', 'Dog'], ['Elephant', 'Egg', 'Emrella'], ['Fridge', 'Finger', 'Fish'], ['Grapes', 'Grass', 'Goat'], ['Hen', 'Heatter', 'Horse'], ['Inter', 'Ink', 'Iron'], ['Job', 'Jangu', 'Jug'], ['Kick', 'Kangaroo', 'Kite'], ['Life', 'Laptop', 'Lemon'], ['Monkey', 'ManoraBeach', 'Mango'], ['News', 'Norway', 'Nose'], ['Orange', 'Owl', 'Oggy'], ['Parrot', 'Papaya', 'Pink'], ['Quice', 'Queen', 'Quote'], ['Rain', 'Red', 'Rat'], ['Sand', 'Samosa', 'Silance'], ['Taxi', 'Tank', 'Tang'], ['Uphone', 'UMER', 'Umrella'], ['Vacume', 'Venus', 'Vanila'], ['Warm', 'Water', 'Watch'], ['Xylem', 'X-ray', 'Xylophone'], ['Yeppy', 'Yang', 'Yellow'], ['Zong', 'Zimbabwe', 'Zebra']]
+    for (var i = 0; i < arrA.length; i++) {
+        if (arrA[i][0][0] === letters) {
+            console.log(arrA[i][number])
+            output.innerText = arrA[i][number]
+            break
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{ }
